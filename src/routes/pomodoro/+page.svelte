d80e5b91051d4c9ed6462f0933e205dfe6b0675b
Bruno Frontini Ganem <brunacho51@gmail.com>
2023-08-15 19:19:08 +0200

    adding pomodoro button

--- src/routes/pomodoro/+page.svelte
+++ src/routes/pomodoro/+page.svelte
@@ -3,7 +3,8 @@
 
 	export let data: PageData;
 
-	$: totalCount = data.workTime * 60 + data.restTime * 60;
+	let totalCount = data.workTime + data.restTime;
+
 	let countdown: number;
 
 	$: if (data.play) {
@@ -14,12 +15,22 @@
 		clearInterval(countdown);
 	}
 
-	let wMins = Math.floor(totalCount / 60);
-	let wSecs = totalCount - wMins * 60;
+	let wMins = Math.floor(totalCount / 60) - 5;
+	let wSecs = totalCount % 60;
+
+	function padTo2Digits(num: number) {
+		return num.toString().padStart(2, '0');
+	}
+	console.log(wMins, wSecs, totalCount, data);
 </script>
 
-<div>
-	<div>
-		{wMins}:{wSecs}
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h1 class="h1">
+			{padTo2Digits(wMins)}:{padTo2Digits(wSecs)}
+		</h1>
+		<div class="flex justify-center space-x-2">
+			<button type="button" class="btn variant-filled">Start</button>
+		</div>
 	</div>
 </div>
